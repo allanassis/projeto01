e546c318b17446fbe52944773435783cef07c8de
Lameck Sandro <lameckanao@gmail.com>
2017-12-08 00:30:46 -0300

    Alterações no layout, e modificação no texto da pagina principal

--- newsite.php
+++ newsite.php
@@ -13,17 +13,17 @@
 		<h1>Facshelp</h1>
 	</header>
 
-	<nav>
-	<ul>
+	<nav id='menu'>
+		<ul>
+		<li><a class='link-text' href='newsite.php'>Home</a></li>
 		<li><a class='link-text' href='materias.php'>Disciplinas</a></li>
-		<li><a class='link-text' href='geometria_analitica.php'>Geometria Analítica</a></li>
-	</ul>
+		<li><a class='link-text' href='contato.php'>Contato</a></li>
+		<li><a class='link-text' href='#'>EM BREVE</a></li>
+		</ul>
 	</nav>
 	<article>
-		<h1>Testando o novo layout</h1>
-		<p>Esse é um exemplo de layout da pagina principal, claro que esses links aqui vão ficar em um menu todo arrumado e bonito</p>
-		<br>
-		<h2>DEU UM PUTA TRAMPO CHEGAR ATE AQ KKKKK</h2>
+		<h1>Seja muito bem vindo</h1>
+		<p><img id='prova' src='todeprova.jpg' alt='to de prova' height='212' width='180'> Olá estudante, se você acessou esse site é porque necessita de ajuda em algo relacionado ao seu curso...<br> Tudo bem, nós te ajudamos.. basta clicar no botão 'disciplinas', escolher a matéria e tirar suas dúvidas.<br> Muito obrigado pela preferência</p>
 	</article>
 
 	<footer>Copyright &copy; LSCF.com</footer>
