4b0f534b5493f5c92c7f1205727b79cf19cc4d5b
Lameck <31391753+lamecksilva@users.noreply.github.com>
2017-12-13 20:38:03 -0300

    Adicionando arquivos
    Trocando menu css e html para um menu com bootstrap

--- newsite.php
+++ newsite.php
@@ -2,6 +2,9 @@
 <html>
 	<head>
 		<link rel='stylesheet' type='text/css' href='all.css'>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		<title>Facshelp</title>
 	</head>
 
@@ -10,21 +13,31 @@
 	<div class='container'>
 
 	<header>
-		<h1>Facshelp</h1>
+		<h1 class='text-header'>Facshelp</h1>
 	</header>
 
-	<nav id='menu'>
-		<ul>
-		<li><a class='link-text' href='newsite.php'>Home</a></li>
-		<li><a class='link-text' href='materias.php'>Disciplinas</a></li>
-		<li><a class='link-text' href='contato.php'>Contato</a></li>
-		<li><a class='link-text' href='#'>Fórum</a></li>
-		<li><a class='link-text' href='#'>ETC</a></li>
+	<nav class='navbar navbar-inverse'>
+		<div class='container-fluid'>
+			<div class='navbar-head'>
+				<a class='navbar-brand' href='#'>Facshelp</a>
+			</div>
+		<ul class='nav navbar-nav'>
+			<li class='active'><a href='#'>Home</a></li>
+			<li class='dropdown'><a class='dropdown-toggle' data-toggle='dropdown' href='#'>Disciplinas<span class='caret'></span></a>
+				<ul class='dropdown-menu'>
+				<li><a href='#'>Algebra Linear</a></li>
+				<li><a href='#'>Calculo 2</a></li>
+				<li><a href='#'>Geometria Analítica</a></li>
+				</ul>
+			</li>
+			<li><a href='#'>Forum</a></li>
+			<li><a href='#'>Contato</a></li>
+			<li><a href='#'>Etc</a></li>
 		</ul>
 	</nav>
 	<article>
-		<h1>Seja muito bem vindo</h1>
-		<p><img id='prova' src='todeprova.jpg' alt='to de prova' height='212' width='180'> Olá estudante, se você acessou esse site é porque necessita de ajuda em algo relacionado ao seu curso...<br> Tudo bem, nós te ajudamos.. basta clicar no botão 'disciplinas', escolher a matéria e tirar suas dúvidas.<br> Muito obrigado pela preferência</p>
+		<h1 class='article-title'>Seja muito bem vindo</h1>
+		<p class='article-text'><img id='prova' src='todeprova.jpg' alt='to de prova' height='212' width='180'>Olá estudante, se você acessou esse site é porque necessita de ajuda em algo relacionado ao seu curso...<br> Tudo bem, nós te ajudamos.. basta clicar no botão 'disciplinas', escolher a matéria e tirar suas dúvidas.<br> Muito obrigado pela preferência</p>
 	</article>
 
 	<footer>Copyright &copy; LSCF.com</footer>
